5465b9aa7886784aa6b1930e107ac52abf29efb2
123456 <hoochanlon@outlook.com>
2023-08-11 13:46:50 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: '《企业基础信息化运维分析实录》',
+    title: '《企业基础信息化运维分析实战》',
     description: '系统主机及网络故障综合处理与信息安全自动化运维方案。',
     base: '/helpdesk-guide/', //项目文件夹设置目录
     head:[
